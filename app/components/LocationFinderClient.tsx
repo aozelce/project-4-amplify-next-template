62d14c54e71b7fe09a66ef8288f96063e86ceabf
aozelce <aozelce@madisoncollege.edu>
2026-01-17 19:17:57 +0300

    Implemented client side API. Added LocationFinderClient and LocationFinderServer components.

---
+++ app/components/LocationFinderClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function LocationFinderServer() {
+    const [locationInfo, setLocationInfo] = useState({City: 'N/A}'});
+
+    async function getLocationInfo() {
+        const response = await fetch("https://apip.cc/api-json/vk.com");
+        console.log(response);
+        const locationData = await response.json();
+        setLocationInfo(locationData);
+    }
+
+    useEffect(() => {
+        getLocationInfo(); 
+    }, []);
+    
+    
+    return (
+    <>
+      {/* Had to use country name instead of city name due to API limitations in my location. */}
+      <h1>Hello from {locationInfo?.CountryName}</h1>
+    
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import "./app.css";
 import ToDo from "./components/ToDo";
+import LocationFinderServer from "./components/LocationFinderServer";
+import LocationFinderClient from "./components/LocationFinderClient";
 
 
 export default function App() {
@@ -8,6 +10,8 @@ export default function App() {
 
   return (
     <main>
+      <LocationFinderClient />
+      <LocationFinderServer />
       <ToDo/>
     </main>
   );
